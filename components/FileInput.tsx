a0bff3392d10b0f9c9aff6a9484047df85cf7e16
Yasa Zaheen <yasazaheen@gmail.com>
2025-03-22 16:04:34 -0400

    File upload handler

--- components/FileInput.tsx
+++ components/FileInput.tsx
@@ -4,10 +4,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { useState } from "react";
 
-export default function FileInput() {
-  const [file, setFile] = useState<File | null>(null);
+import { createClient } from "@/utils/supabase/client";
 
-  // const supabase = createClient();
+// Create Supabase client
+
+export default function FileInput() {
+  const supabase = createClient();
+  const [file, setFile] = useState<File | null>(null);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files ? e.target.files[0] : null;
@@ -17,8 +20,20 @@ export default function FileInput() {
   const handleUpload = async () => {
     const formData = new FormData();
 
+    console.log("Uploading");
+
     if (file) {
       formData.append("file", file);
+    }
+
+    // Upload file using standard upload. Change this to a server action later on.
+    const { data, error } = await supabase.storage
+      .from("files")
+      .upload(`${file?.name}`, file!);
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Success");
     }
   };
 
